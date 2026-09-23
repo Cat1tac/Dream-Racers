db74efec48decd9277d2dda866bb7392b947d9ef
Juexis <77648644+Juexis@users.noreply.github.com>
2026-09-23 16:29:54 -0400

    transitions for main menu and character select
    - forward and backward transitions for main menu and character select
    
    - old screen awaits animation finished to hide, but new screen can begin entering synchronously
    
    - plus a transition buffer timer so new screen entering doesn't overlap with old screen leaving

--- src/ui/scripts/ui_manager.gd
+++ src/ui/scripts/ui_manager.gd
@@ -17,11 +17,14 @@ func _ready() -> void:
 	if main_menu.visible:
 		current_state = State.MAIN_MENU
 
-#region transitions
+#region forward transitions
 func _on_race_button_pressed() -> void:
 	current_state = State.CHAR_SELECT
 	main_menu.transition_to_state(character_select)
 
+#endregion
+
+#region back transitions
 func _on_char_back_button_pressed() -> void:
 	current_state = State.MAIN_MENU
 	character_select.transition_to_state(main_menu)
--- src/ui/scripts/ui_state.gd
+++ src/ui/scripts/ui_state.gd
@@ -6,9 +6,32 @@ extends Control
 
 signal transition_finished
 
+# NOTE each UIState node must have "exit_left" "enter_left"
+@export var transition_animations: AnimationPlayer
+
+@export var next_state: UIState
+@export var previous_state: UIState
+
 func _ready() -> void:
 	pass
 
 func transition_to_state(newState: UIState) -> void:
-	hide()
+	## exit old state
+	if newState == next_state: # going forward a state
+		transition_animations.play("exit_left")
+	else: # going back a state
+		transition_animations.play("exit_right")
+	
 	newState.show()
+	
+	## enter newState
+	if newState == next_state: # going forward a state
+		await get_tree().create_timer(0.3).timeout # transition buffer timer
+		newState.transition_animations.play("enter_right")
+	else: # going back a state
+		await get_tree().create_timer(0.3).timeout
+		newState.transition_animations.play("enter_left")
+	
+	# hide old screen
+	await transition_animations.animation_finished
+	hide()
